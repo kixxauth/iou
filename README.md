ea904d1ee400ddc133ac151a48d489abf79895a4
Kristoffer Walker <kris@kixx.name>
2013-02-20 00:02:06 -0500

    Initial commit
    	new file:   .gitignore
    	new file:   LICENSE
    	new file:   README.md
    	new file:   index.js

---
+++ README.md
@@ -0,0 +1,12 @@
+IOU
+===
+
+A simple JavaScript library which forces asynchronous code to give you a
+promise for a value; at the very least. Really, that's not asking too much is
+it?
+
+Copyright and License
+---------------------
+Copyright (c) 2013 by Kris Walker <kris@kixx.name> (http://www.kixx.name).
+
+Unless otherwise indicated, all source code is licensed under the MIT license. See LICENSE for details.
---
+++ index.js
@@ -0,0 +1,109 @@
+function newDefer() {
+    var self = {}
+      , PENDING = 'pending'
+      , KEPT = 'kept'
+      , FAILED = 'failed'
+      , status = PENDING
+      , knownFate
+      , keeperQueue = newQueue()
+      , failureQueue = newQueue()
+
+    function newQueue() {
+        var q = []
+
+        function queue(wrapped) {
+            q.push(wrapped);
+        }
+
+        queue.commit = function (val) {
+            enlighten(q, val);
+        };
+
+        return queue;
+    }
+
+    function enlighten(handlers, val) {
+        var i = 0
+          , len = handlers.length
+
+        for (; i < len; i += 1) {
+            handlers[i](val);
+        }
+    }
+
+    function commit(state, val) {
+        if (status !== PENDING) return;
+
+        status = state;
+        knownFate = val;
+
+        if (status === KEPT) {
+            keeperQueue.commit(val);
+        } else {
+            failureQueue.commit(val);
+        }
+    }
+
+    function wrapHandler(handler, deferred) {
+        function wrappedHandler(val) {
+            process.nextTick(newWrapper(handler, deferred, val));
+        }
+        return wrappedHandler;
+    }
+
+    function newWrapper(handler, deferred, val) {
+        function nextLayer() {
+            try {
+                var result = handler(val);
+                if (result && result.isPromise === true) {
+                    result.then(deferred.keep);
+                    result.failure(deferred.fail);
+                } else {
+                    deferred.keep(result);
+                }
+            } catch (err) {
+                deferred.fail(err);
+            }
+        }
+
+        return nextLayer;
+    }
+
+    self.keep = function (val) {
+        commit(KEPT, val);
+        return this;
+    };
+
+    self.fail = function (err) {
+        commit(FAILED, err);
+        return this;
+    };
+
+    self.promise = {
+        then: function (next) {
+            var deferred = newDefer()
+              , wrapped = wrapHandler(next, deferred)
+
+            if (status === 'pending') {
+                keeperQueue(wrapped);
+            } else {
+                wrapped(knownFate);
+            }
+            return deferred.promise;
+        }
+
+      , failure: function (handler) {
+            var deferred = newDefer()
+              , wrapped = wrapHandler(handler, deferred)
+
+            if (status === 'pending') {
+                failureQueue(wrapped);
+            } else {
+                wrapped(knownFate);
+            }
+            return deferred.promise;
+        }
+    };
+
+    return self;
+}
