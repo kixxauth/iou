b103a25ed1d814e071b0d2f3bb2ee51129b2116c
Kristoffer Walker <kris@kixx.name>
2013-12-26 10:19:55 -0500

    aplus test adapter
     On branch v_2_ecma6
    
    	modified:   test/aplus-adapter.js

--- test/aplus-adapter.js
+++ test/aplus-adapter.js
@@ -1,3 +1,15 @@
-exports.resolved = null;
-exports.rejected = null;
-exports.deferred = null;
+var IOU = require('../index')
+
+
+exports.resolved = IOU.Promise.resolve;
+
+exports.rejected = IOU.Promise.reject;
+
+exports.deferred = function () {
+  var deferred = {}
+  deferred.promise = new IOU.Promise(function (resolve, reject) {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+  return deferred;
+};
