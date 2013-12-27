42a8a533c5fd08c845ad567c3879d1f1bdefb4a3
Kristoffer Walker <kris@kixx.name>
2013-12-27 05:44:38 -0500

    Running aplus tests up through 2.2.7
     On branch v_2_ecma6
    
    	modified:   index.js
    	modified:   test/aplus-adapter.js

--- index.js
+++ index.js
@@ -1,250 +1,117 @@
+var FULFILLED = 'fulfilled'
+  , REJECTED = 'rejected'
+  , PENDING = 'pending'
+
+
 function Promise(block) {
-  if (!(this instanceof Promise)) {
-    throw new TypeError("Failed to construct 'Promise': Please use the 'new' operator, this object constructor cannot be called as a function.");
-  }
-  if (typeof block !== 'function') {
-    throw new TypeError("You must pass a resolver function as the first argument to the promise constructor.");
-  }
+  var deferred = newDefer(this)
 
-  var deferred = exports.newDefer()
+  this.then = function (onFulfilled, onRejected) {
+    var promise = new Promise(function (resolve, reject) {
+      if (typeof onFulfilled === 'function') {
+        deferred.onFulfilled(wrapHandler(onFulfilled, promise, resolve, reject));
+      } else {
+        deferred.onFulfilled(wrapProxy(resolve));
+      }
+      if (typeof onRejected === 'function') {
+        deferred.onRejected(wrapHandler(onRejected, promise, resolve, reject));
+      } else {
+        deferred.onRejected(wrapProxy(reject));
+      }
+    });
 
-  block(deferred.keep, deferred.fail);
+    return promise;
+  };
 
-  return deferred.promise;
+  block(deferred.resolve, deferred.reject);
 }
 
 Promise.resolve = function (value) {
-  var promise = new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     resolve(value);
   });
-  return promise;
 };
 
 Promise.reject = function (error) {
-  var promise = new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     reject(error);
   });
-  return promise;
 };
+
+function newDefer(promise) {
+  var self = Object.create(null)
+    , status = PENDING
+    , knownFate
+    , fulfillmentHandlers = []
+    , rejectionHandlers = []
+
+  self.resolve = function (value) {
+    process.nextTick(function () {
+      if (status !== PENDING) return;
+      status = FULFILLED;
+      knownFate = value;
+      fulfillmentHandlers.forEach(function (handler) {
+        handler(status, knownFate);
+      });
+    });
+  };
+
+  self.reject = function (error) {
+    process.nextTick(function () {
+      if (status !== PENDING) return;
+      status = REJECTED;
+      knownFate = error;
+      rejectionHandlers.forEach(function (handler) {
+        handler(status, knownFate);
+      });
+    });
+  };
+
+  self.onFulfilled = function (handler) {
+    if (status === PENDING) {
+      fulfillmentHandlers.push(handler);
+    } else if (status === FULFILLED) {
+      process.nextTick(function () {
+        handler(knownFate);
+      });
+    }
+  };
+
+  self.onRejected = function (handler) {
+    if (status === PENDING) {
+      rejectionHandlers.push(handler);
+    } else if (status === REJECTED) {
+      process.nextTick(function () {
+        handler(knownFate);
+      });
+    }
+  };
+
+  return self;
+}
 
 exports.Promise = Promise;
 
-exports.newDefer = function newDefer() {
-  var self = {}
 
-    // Possible states:
-    , PENDING = 'pending'
-    , KEPT = 'kept'
-    , FAILED = 'failed'
-
-    // Current state:
-    , status = PENDING
-
-    // The resolved value (assigned as soon as we know what it will be).
-    , knownFate
-
-    // Handler queues for keeper handlers and failure handlers.
-    , keeperQueue = newQueue('keepers')
-    , failureQueue = newQueue('failures')
-
-
-  // Helper function: Resolves this deferred such that it cannot be resolved
-  // again, and notifies all the handlers.
-  function commit(state, val) {
-    if (status !== PENDING) return;
-
-    status = state; // Update the state for this deferred.
-    knownFate = val; // Set the value for the knownFate of this deferred.
-
-    // Notify the relevant handler queue.
-    if (status === KEPT) {
-      keeperQueue.commit(knownFate);
-    } else {
-      failureQueue.commit(knownFate);
+function wrapHandler(handler, promise, resolve, reject) {
+  return function (state, fate) {
+    var x
+    try {
+      x = handler(fate);
+    } catch (err) {
+      reject(err);
+      return;
     }
-  }
-
-  self.keep = function (val) {
-    commit(KEPT, val);
-    return this;
-  };
-
-  self.fail = function (err) {
-    commit(FAILED, err);
-    return this;
-  };
-
-  self.promise = {
-    isPromise: true
-
-    , then: function (next, handler) {
-        var deferred = newDefer()
-
-        switch (status) {
-
-        case PENDING:
-          keeperQueue(wrapHandler(deferred, next));
-
-          if (handler) {
-            failureQueue(wrapHandler(deferred, handler));
-          }
-
-          // Add a dummy failure handler to the failureQueue for this
-          // promise layer; proxying it to the next layer.
-          failureQueue(function (err) {
-            process.nextTick(function () {
-              deferred.fail(err);
-            });
-          });
-          break;
-
-        case KEPT:
-          // Create and immediately call the wrapped success handler.
-          wrapHandler(deferred, next)(knownFate);
-          break;
-
-        case FAILED:
-          if (handler) {
-            // Create and immediately call the wrapped handler.
-            wrapHandler(deferred, handler)(knownFate);
-
-          } else {
-            // If the deferred has already been resolved, then proxy it to
-            // the next layer.
-            deferred.fail(knownFate);
-          }
-          break;
-        }
-
-        return deferred.promise;
-      }
-
-    , failure: function (handler) {
-        var deferred = newDefer()
-
-        switch (status) {
-
-        case PENDING:
-          failureQueue(wrapHandler(deferred, handler));
-
-          // Add a dummy keeper handler to the keeperQueue for this
-          // promise layer; proxying it to the next layer.
-          keeperQueue(function (val) {
-            process.nextTick(function () {
-              deferred.keep(val);
-            });
-          });
-          break;
-
-        case FAILED:
-          // Create and immediately call the wrapped handler.
-          wrapHandler(deferred, handler)(knownFate);
-          break;
-
-        case KEPT:
-          // If the deferred has already been resolved, then proxy it to
-          // the next layer.
-          deferred.keep(knownFate);
-          break;
-        }
-
-        return deferred.promise;
-      }
-  };
-
-  // Freeze the promise so that it can be passed around to other code, and
-  // also to protect the .isPromise flag.
-  Object.freeze(self.promise);
-  return self;
-};
-
-exports.waitFor = function waitFor(promises) {
-  if (!Array.isArray(promises)) {
-    promises = Array.prototype.slice.call(arguments);
-  }
-
-  var deferred = exports.newDefer()
-    , values = []
-    , count = 0
-    , expected = promises.length
-
-  promises.forEach(function (promise, index) {
-    promise.then(function (val) {
-      values[index] = val;
-      if ((count += 1) === expected) {
-        deferred.keep(values);
-      }
-    }, deferred.fail);
-  });
-
-  return deferred.promise
-};
-
-
-// Create a new function queue. This returns a function, which accepts a
-// function as the only parameter, adding it to the queue. The returned
-// function object also has a .commit() method which will execute all functions
-// in the queue with the given value.
-function newQueue(name) {
-  var q = []
-
-  function queue(func) {
-    q.push(func);
-  }
-
-  queue.commit = function (val) {
-    callFunctionsWith(q, val);
-  };
-
-  return queue;
-}
-
-
-// Execute an array of functions with the given value.
-function callFunctionsWith(funcs, val) {
-  var i = 0
-    , len = funcs.length
-
-  for (; i < len; i += 1) {
-    funcs[i](val);
+    commitPromise(promise, resolve, reject, x);
   }
 }
 
+function wrapProxy(next) {
+  return function (state, fate) {
+    next(fate);
+  };
+}
 
-// Wrap a handler passed to promise.then() or promise.failure(). Returns a
-// handler function which accepts a single parameter (the 'value') and is
-// suitable for passing into a newQueue.
-function wrapHandler(deferred, handler) {
-  function wrappedHandler(val) {
-
-    // Promise handlers must execute on the next tick of the event loop.
-    process.nextTick(function () {
-      try {
-        var rv = handler(val);
-      } catch (e) {
-        // If the handler throws, the next promise.failure() handler
-        // must be called.
-        deferred.fail(e);
-        return;
-      }
-
-      if (rv && rv instanceof Error) {
-        // If the handler returns an error, the next promise.failure()
-        // handler must be called.
-        deferred.fail(rv);
-
-      } else if (rv && rv.isPromise) {
-        // If the handler returns a promise, then proxy it.
-        rv.then(deferred.keep);
-        rv.failure(deferred.fail);
-
-      } else {
-        // Otherwise pass along the resolved value to the next
-        // promise.then() handler.
-        deferred.keep(rv);
-      }
-    });
-  }
-
-  return wrappedHandler;
+function commitPromise(promise, resolve, reject, x) {
+  resolve(x);
 }
