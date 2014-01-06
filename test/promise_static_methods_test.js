74cadeb03632241dd6a9080cdbd06ece72b0b001
Kristoffer Walker <kris@kixx.name>
2014-01-06 09:02:03 -0500

    Tests for Promise.reject()
     On branch v_2_ecma6
    
    	modified:   test/promise_static_methods_test.js

--- test/promise_static_methods_test.js
+++ test/promise_static_methods_test.js
@@ -23,9 +23,8 @@ exports["Promise.resolve()"] = {
       resolve(DUMMY);
     });
       
-    var newPromise = this.resolve(DUMMY)
+    var newPromise = this.resolve(promise)
 
-    test.ok(newPromise instanceof this.Promise, "newPromise instanceof Promise");
     test.ok(newPromise !== promise, "newPromise !== promise");
     return test.done();
   },
@@ -76,5 +75,79 @@ exports["Promise.resolve()"] = {
     }
       
     this.resolve().then(onSuccess, onFailure);
+  }
+};
+
+exports["Promise.reject()"] = {
+  setUp: function(done) {
+    this.Promise = IOU.Promise;
+    this.reject = this.Promise.reject;
+    return done();
+  },
+
+  "creates a Promise instance from plain Error": function (test) {
+    var promise = this.reject(ERROR)
+    test.ok(promise instanceof this.Promise, "promise instanceof this.Promise");
+    return test.done();
+  },
+
+  "creates a *new* Promise from an existing Promise": function (test) {
+    var promise = new this.Promise(function (resolve, reject) {
+      reject(ERROR);
+    });
+      
+    var newPromise = this.reject(promise)
+
+    test.ok(newPromise !== promise, "newPromise !== promise");
+    return test.done();
+  },
+
+  "creates a Promise for ERROR that rejects to ERROR": function (test) {
+    test.expect(1);
+
+    function onSuccess() {
+      test.ok(false, 'onSuccess() should not be called');
+      test.done();
+    }
+
+    function onFailure(val) {
+      test.strictEqual(val, ERROR);
+      test.done();
+    }
+      
+    this.reject(ERROR).then(onSuccess, onFailure);
+  },
+
+  "creates a Promise for THENABLE which rejects to THENABLE": function (test) {
+    test.expect(1);
+    var thenable = THENABLE(DUMMY);
+
+    function onSuccess() {
+      test.ok(false, 'onSuccess() should not be called');
+      test.done();
+    }
+
+    function onFailure(reason) {
+      test.strictEqual(reason, thenable);
+      test.done();
+    }
+      
+    this.reject(thenable).then(onSuccess, onFailure);
+  },
+
+  "creates a Promise for rejection reason `undefined` when no arguments": function (test) {
+    test.expect(1);
+
+    function onSuccess() {
+      test.ok(false, 'onSuccess() should not be called');
+      test.done();
+    }
+
+    function onFailure(reason) {
+      test.strictEqual(reason, undefined);
+      test.done();
+    }
+      
+    this.reject().then(onSuccess, onFailure);
   }
 };
